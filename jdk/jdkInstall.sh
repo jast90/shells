c11d0127262897a7359cb95b0f2ed7b7c82f4f66
jast90 <zhangzhiwen91@gmail.com>
2020-06-17 16:46:40 +0800

    cdh，安装JDK必须安装到该目录下/usr/share/java目录下

--- jdk/jdkInstall.sh
+++ jdk/jdkInstall.sh
@@ -1,7 +1,8 @@
-# 安装jdk
-sudo tar -zxvf /opt/software/jdk-8u212-linux-x64.tar.gz -C /opt
-# 通过sed 将export JAVA_HOME=/opt/jdk1.8.0_212 追加到文件/etc/profile末尾
-sudo sed -i '$ i export JAVA_HOME=/opt/jdk1.8.0_212' /etc/profile
+# 安装jdk 。注意：cdh，安装JDK必须安装到该目录下/usr/share/java目录下
+mkdir /usr/share/java
+sudo tar -zxvf /opt/software/jdk-8u212-linux-x64.tar.gz -C /usr/share/java
+# 通过sed 将export JAVA_HOME=/usr/share/java/jdk1.8.0_212 追加到文件/etc/profile末尾
+sudo sed -i '$ i export JAVA_HOME=/usr/share/java/jdk1.8.0_212' /etc/profile
 # 通过sed 将export export PATH=$JAVA_HOME/bin:$PATH 追加到文件/etc/profile末尾
 sudo sed -i '$ i export PATH=$JAVA_HOME/bin:$PATH' /etc/profile
 source /etc/profile
