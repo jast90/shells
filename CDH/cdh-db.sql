fc55f1393ec582d2204d95a657ae7e1b131e54fd
jast90 <zhangzhiwen91@gmail.com>
2020-06-17 12:48:50 +0800

    drop database if exists

--- CDH/cdh-db.sql
+++ CDH/cdh-db.sql
@@ -1,2 +1,4 @@
+drop database if exists amon;
 create database amon default charset utf8 collate utf8_general_ci;
+drop database if exists hive;
 create database hive default charset utf8 collate utf8_general_ci;
