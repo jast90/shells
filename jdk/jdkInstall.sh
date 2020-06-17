ee0ead266bb42d8133676005f9e8b14d17360255
jast90 <zhangzhiwen91@gmail.com>
2020-06-17 12:37:56 +0800

    jdk install 添加注释

--- jdk/jdkInstall.sh
+++ jdk/jdkInstall.sh
@@ -1,5 +1,7 @@
 # 安装jdk
 sudo tar -zxvf /opt/software/jdk-8u212-linux-x64.tar.gz -C /opt
+# 通过sed 将export JAVA_HOME=/opt/jdk1.8.0_212 追加到文件/etc/profile末尾
 sudo sed -i '$ i export JAVA_HOME=/opt/jdk1.8.0_212' /etc/profile
-sudo sed -i '$ i export PATH=$JAVA_HOME:$PATH' /etc/profile
-sudo source /etc/profile
+# 通过sed 将export export PATH=$JAVA_HOME/bin:$PATH 追加到文件/etc/profile末尾
+sudo sed -i '$ i export PATH=$JAVA_HOME/bin:$PATH' /etc/profile
+source /etc/profile
