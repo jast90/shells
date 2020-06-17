bbcd4bd94436fb22b24d9040cd0c30e9c78162e2
jast90 <zhangzhiwen91@gmail.com>
2020-06-17 16:38:41 +0800

    mkdir

--- jdk/jdkDownloader.sh
+++ jdk/jdkDownloader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 jdkURL="https://github.com/frekele/oracle-java/releases/download/8u212-b10/jdk-8u212-linux-x64.tar.gz"
-
+sudo mkdir /opt/software
 cd /opt/software
 sudo wget -b $jdkURL
