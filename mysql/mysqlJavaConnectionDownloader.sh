62d0992f7499b7a26cdf85181be3c1472d08afd8
jast90 <zhangzhiwen91@gmail.com>
2020-06-19 19:25:23 +0800

    mysql connection

--- mysql/mysqlJavaConnectionDownloader.sh
+++ mysql/mysqlJavaConnectionDownloader.sh
@@ -2,4 +2,9 @@
 
 connectorJava="https://mirrors.sohu.com/mysql/Connector-J/mysql-connector-java-5.1.48.zip"
 
+sudo mkdir /opt/software
+cd /opt/software
+sudo mkdir mysql
+cd /opt/software/mysql
+
 sudo wget -b $connectorJava
